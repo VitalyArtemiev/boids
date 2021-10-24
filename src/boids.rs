e70b200444fd8944b8e4d1558a0ca2985dd545b3
Виталий Артемьев <vitaly.artemiev@yandex.ru>
2021-10-24 06:23:27 +0300

    improved collision parameters somewhat

--- src/boids.rs
+++ src/boids.rs
@@ -34,7 +34,7 @@ const SPREAD: f64 = 600.;
 const VEL_SPREAD: f64 = 500.;
 const ACC_MAX: f64 = 1000.;
 const VEL_MAX: f64 = 100.;
-const DIST_REPEL: f64 = 100.;
+const DIST_REPEL: f64 = 20.;
 const FORMATION_SPACING: f64 = 24.;
 
 impl BoidVec {
@@ -83,26 +83,10 @@ impl BoidVec {
         let form_width = (rlen / FORMATION_SPACING).round() as usize;
 
         for cur in 0..self.len() {
-            /*match self.state[cur] {
-                Stationary => continue,
-                Marching => {}
-            }*/
-
             let target_offset = Vec2f {
                 x: cur.checked_rem(form_width).unwrap_or_default() as f64,
                 y: cur.checked_div(form_width).unwrap_or_default() as f64,
             }.rot_align(rvec) * FORMATION_SPACING;
-
-            //println!("{:?}", target_offset);
-
-            /*let target_offset = if form_width != 0 {
-                Vec2f {
-                    x: (cur % form_width) as f64,
-                    y: (cur / form_width) as f64,
-                } * 50.
-            } else {
-                Vec2f::default()
-            };*/
 
             let target = p.r1 + target_offset;
             let mut d = target - self.pos[cur];
@@ -111,30 +95,34 @@ impl BoidVec {
             if dist < 1. {
                 self.state[cur] = Stationary;
                 self.vel[cur] = Vec2f::default();
-                continue;
+                //continue;
             } else {
                 self.state[cur] = Marching
             }
 
-            /*for i in 0..self.len() {
-                let vec = self.pos[cur] - self.pos[i];
-                let dist = (vec.len() + 0.1).ln() - 4.;
+            d.clamp(ACC_MAX / 10.);
 
-                d += vec * (-dist);
-            }*/
+            for i in 0..self.len() {//todo: this loop is problematic
+                let vec = self.pos[cur] - self.pos[i];
+                //let dist = (vec.len() + 0.1).ln() - 4.;
+
+                if vec.len() < DIST_REPEL {
+                    d += vec * (500.01 / self.len() as f64) /* * (-dist)*/;
+                }
+            }
 
             d.clamp(ACC_MAX);
 
             self.vel[cur] += d * dt;
             self.vel[cur].clamp( VEL_MAX.min(dist) );
 
+            self.pos[cur] += self.vel[cur] * dt;
+
             let heading: f64 = f64::atan2(self.vel[cur].y, self.vel[cur].x);
 
             if heading.is_normal() {
                 self.r[cur] = heading;
             }
-
-            self.pos[cur] += self.vel[cur] * dt;
         }
     }
 }
